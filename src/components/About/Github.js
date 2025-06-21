8859e598bff94c7dda5b2a897021ba9198a6acb6
jovani john <jovanijohn40@gmail.com>
2025-06-21 12:11:31 +0300

    Fix unused imports and update netlify config

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,5 +1,3 @@
-import React from "react";
-import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
 function Github() {
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 // import { CgCPlusPlus } from "react-icons/cg";
-import { DiJavascript1, DiReact, DiPython, DiJava } from "react-icons/di";
+import { DiJavascript1, DiReact, DiPython } from "react-icons/di";
 import { FaCss3, FaHtml5, FaGitSquare } from "react-icons/fa";
 import {
   SiTypescript,
